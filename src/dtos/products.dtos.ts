8c8a73e85510c466350e674a5c80e5fc15b8f899
macgbluedev <macgbluedev@users.noreply.github.com>
2022-03-26 00:53:19 +0100

    Add validation layer in runtime, include restrictions on dtos

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,33 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types'
+
 export class CreateProductDto {
-    readonly name: string;
-    readonly description: string;
-    readonly price: number;
-    readonly stock: number;
-    readonly image: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
+  readonly image: string;
 }
 
-export class UpdateProductDto {
-    readonly name?: string;
-    readonly description?: string;
-    readonly price?: number;
-    readonly stock?: number;
-    readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
