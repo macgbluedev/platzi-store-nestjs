1d68a333869f356b0f1211d933c98757f062766d
macgbluedev <macgbluedev@users.noreply.github.com>
2022-03-28 03:53:59 +0200

    Add config with type

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,13 @@ import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 
 import { enviroments } from '../enviroments';
+import config from 'config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     UsersModule, 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from '../config'
 
 @Injectable()
 export class AppService {
   constructor(
     //@Inject('API_KEY') private apiKey: string,
-    @Inject('TASKS') private tasks: any[],
-    private configService: ConfigService
+    //@Inject('TASKS') private tasks: any[],
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY');
-    const dbName = this.configService.get('DATABASE_NAME');
-    return `${apiKey},${dbName}`;
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
+    return `Hola ${apiKey},${dbName}`;
   }
 }
