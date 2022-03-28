49aa97c78edf6c1a0910b599f94cd6dd165842b7
macgbluedev <macgbluedev@users.noreply.github.com>
2022-03-29 01:44:40 +0200

    add mysql and phpmyadmin service with docker; adapt mysql in database.module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,9 +14,9 @@ const API_KEY_PROD = 'prod_12345';
     TypeOrmModule.forRootAsync({
       inject: [config.KEY],
       useFactory:( configService: ConfigType<typeof config>) => {
-        const { user, host, dbName, password, port } = configService.postgres;
+        const { user, host, dbName, password, port } = configService.mysql;
         return {
-          type: 'postgres',
+          type: 'mysql',
           host,
           port,
           username: user,
