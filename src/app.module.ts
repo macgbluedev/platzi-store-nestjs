3adc1c090a9e87ef820934892ed91eef59d79c88
macgbluedev <macgbluedev@users.noreply.github.com>
2022-03-28 03:16:59 +0200

    Global module; config module with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { firstValueFrom } from 'rxjs';
+import { DatabaseModule } from './database/database.module';
 
-const API_KEY = 'dev_12345';
-const API_KEY_PROD = 'prod_12345';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    UsersModule, 
+    ProductsModule, 
+    HttpModule, 
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
         const tasks = await http.get(
           'https://jsonplaceholder.typicode.com/todos',
         );
-
         return await (
           await firstValueFrom(tasks)
         ).data;
