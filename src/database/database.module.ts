832715f77c4f83f7c0ac27c8cb92a93ed4cf08fb
macgbluedev <macgbluedev@users.noreply.github.com>
2022-03-29 18:38:13 +0200

    manage migrations with typeorm ; desactive synchronize

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -22,7 +22,7 @@ const API_KEY_PROD = 'prod_12345';
           username: user,
           password,
           database: dbName,
-          synchronize: true,
+          synchronize: false,
           autoLoadEntities: true,
         };
       },
